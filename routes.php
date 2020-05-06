f2f767a32e6e3e427c02377969d5cb4c3a62aff0
Leo Scherer <lenoxy@gmx.ch>
2020-05-06 12:08:11 +0200

    WIP

--- routes.php
+++ routes.php
@@ -3,5 +3,10 @@
 $router = new Router();
 
 $router->define([
-    '' => 'app/Controllers/ShowController.php'
+    '' => 'app/Controllers/ShowController.php',
+    'create' => 'app/Controllers/CreateController.php',
+    'edit' => 'app/Controllers/EditController.php',
+
+    'handle-create' => 'app/Controllers/HandleCreateController.php',
+    'handle-edit' => 'app/Controllers/HandleEditController.php'
 ]);
