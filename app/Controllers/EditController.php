9e86d744298550684a17f24a12257810d74d828f
Leo Scherer <lenoxy@gmx.ch>
2020-05-06 15:57:54 +0200

    Import fix

--- app/Controllers/EditController.php
+++ app/Controllers/EditController.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once 'app/Controllers/ValidationHandleController.php';
-
 if ($_SERVER['REQUEST_METHOD'] === 'GET' || $_SERVER['REQUEST_METHOD'] === 'POST') {
     // Form uses GET the first time. After
     $id = isset($_GET['id']) ? $_GET['id'] : $_POST['id'];
